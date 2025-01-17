cbba95202296d34783a9b3e1ef29c46e9de4cb08
Krasnovskiy Denis <denredsky@mail.ru>
2025-01-17 11:04:17 +0300

    - added some linting plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,29 @@
-import js from '@eslint/js';
+import eslint from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tsEslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import sonarjs from 'eslint-plugin-sonarjs';
+import importPlugin from 'eslint-plugin-import';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default tsEslint.config(
+  { ignores: ['dist', 'eslint.config.js'] },
+  eslint.configs.recommended,
+  tsEslint.configs.recommendedTypeChecked,
+  sonarjs.configs.recommended,
+  // importPlugin.flatConfigs.recommended,
+  // importPlugin.flatConfigs.typescript,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
     }
-  }
+  },
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  jsxA11y.flatConfigs.recommended
 );
