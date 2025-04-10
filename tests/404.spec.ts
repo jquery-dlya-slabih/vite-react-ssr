6f9dff3ebf9b6fc13ac0042f82d8280a710551dc
Krasnovskiy Denis <denredsky@mail.ru>
2025-04-10 13:47:52 +0300

    - e2e test fixed

--- tests/404.spec.ts
+++ tests/404.spec.ts
@@ -5,7 +5,7 @@ test('Not found path', async ({ page }) => {
 
   await expect(page.locator('h1')).toContainText('404');
   await expect(page.locator('#root')).toContainText('Ooops page not found');
-  await page.getByRole('link', { name: 'To main page...' }).click();
+  await page.getByRole('link', { name: 'To main page' }).click();
 
   await expect(page.getByRole('heading', { name: 'touché choice' })).toBeVisible();
 });
